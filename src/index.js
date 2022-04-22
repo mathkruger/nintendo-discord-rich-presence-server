4b634b92bfa3df92656e3853fa167356d7499947
Matheus Kruger <ms-kruger@hotmail.com>
2022-04-21 21:04:02 -0300

    adding api module

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
-const express = require('express');
-const cors = require('cors');
-const routes = require('./routes');
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(routes);
-
+const app = require("./api");
 app.listen(process.env.PORT || 3333);
