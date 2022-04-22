9f8996a6f7d3dbf3f6696996143e9328b91f1938
Matheus Kruger <ms-kruger@hotmail.com>
2022-04-22 13:14:59 -0300

    removing twitch api, using nintendo switch eshop api instead

--- src/controllers/game.controller.js
+++ src/controllers/game.controller.js
@@ -1,28 +1,11 @@
-const axios = require('axios');
-const app_config = require('../../config');
+const { getQueriedGamesBrazil } = require('nintendo-switch-eshop');
 
 module.exports = {
     async searchGame(request, response) {
         try {
             const searchTerm = request.query.term;
-            const authorization = request.headers['authorization'];
-
-            const data = await axios({
-                url: 'https://api.igdb.com/v4/games',
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Client-ID': app_config.client_id,
-                    'Authorization': authorization,
-                },
-                data: `
-                search "${searchTerm}";
-                fields name,rating,cover;
-                where release_dates.platform = (130);
-            `
-            });
-
-            return response.json(data.data);
+            const result = await getQueriedGamesBrazil(searchTerm);
+            return response.json(result);
         } catch (error) {
             return response.status(500).send({
                 success: false,
@@ -30,33 +13,4 @@ module.exports = {
             });
         }
     },
-
-    async getGameCover(request, response) {
-        try {
-            const id = request.query.id;
-            const authorization = request.headers['authorization'];
-
-            const data = await axios({
-                url: 'https://api.igdb.com/v4/covers',
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Client-ID': app_config.client_id,
-                    'Authorization': authorization,
-                },
-                data: `
-                    fields url;
-                    where id = ${id};
-                `
-            });
-
-            return response.json(data.data[0]);
-
-        } catch (error) {
-            return response.status(500).send({
-                success: false,
-                error
-            });
-        }
-    }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const AuthController = require('./controllers/auth.controller');
 const GameController = require('./controllers/game.controller');
 const DiscordController = require('./controllers/discord.controller');
 
 const routes = express.Router();
 
-routes.get('/auth/token', AuthController.getToken);
 routes.get('/game/info', GameController.searchGame);
-routes.get('/game/cover', GameController.getGameCover);
 routes.get('/discord/update', DiscordController.updatePresence);
 
 module.exports = routes;
